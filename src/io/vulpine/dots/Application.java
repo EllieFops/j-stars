5ae79474861d2630578604e45d7a3f8e2eb0a85e
Ellie Harper <elliefops@gmail.com>
2016-03-31 06:49:05 -0400

    Yesssss

---
+++ src/io/vulpine/dots/Application.java
@@ -0,0 +1,120 @@
+package io.vulpine.dots;
+
+import javafx.animation.AnimationTimer;
+import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application
+{
+  public static final int MAX_X           = 1920;
+  public static final int MAX_Y           = 1068;
+  public static final int DOT_COUNT       = 100;
+  public static final int MAX_LINE_LENGTH = 150;
+  public static final int MAX_CONNECTIONS = 4;
+
+  public static final int DOT_WIDTH = 3;
+
+  public static void main( String[] args )
+  {
+    launch(args);
+  }
+
+  @Override
+  public void start( final Stage stage ) throws Exception
+  {
+    final Canvas          canvas1, canvas2;
+    final Dot[]           dots;
+    final GraphicsContext gc;
+    final Group           g;
+    final Scene           s;
+    final AnimationTimer timer;
+
+    stage.setTitle("Dots");
+
+    dots = new Dot[DOT_COUNT];
+    canvas1 = new Canvas(MAX_X, MAX_Y);
+    canvas2 = new Canvas(MAX_X, MAX_Y);
+    gc = canvas1.getGraphicsContext2D();
+    gc.setGlobalBlendMode(BlendMode.SRC_OVER);
+    canvas2.getGraphicsContext2D().setFill(Color.BLACK);
+    canvas2.getGraphicsContext2D().fillRect(0, 0, MAX_X, MAX_Y);
+    g = new Group(canvas2);
+    g.getChildren().add(canvas1);
+    s = new Scene(g);
+
+    stage.setWidth(800);
+    stage.setHeight(600);
+
+    stage.setScene(s);
+    stage.show();
+
+    for (int i = 0; i < DOT_COUNT; i++) {
+      dots[i] = new Dot();
+    }
+
+    timer = new AnimationTimer() {
+
+      @Override
+      public void handle( final long now )
+      {
+        gc.clearRect(0, 0, MAX_X, MAX_Y);
+
+        // Position
+        for (int i = 0; i < DOT_COUNT; i++) {
+          final Point2D cur;
+          final double  x, y;
+
+          dots[i].step();
+          cur = dots[i].getCurrent();
+
+          x = cur.getX();
+          y = cur.getY();
+
+          if (x > MAX_X || x < 0 || y > MAX_Y || y < 0) {
+            dots[i] = new Dot();
+          }
+        }
+
+        // Render
+        for (Dot d : dots) {
+          final Point2D cur;
+          final double  cx, cy;
+          int connections = 0;
+
+          gc.setGlobalAlpha(d.getOpacity());
+          gc.setFill(Color.RED);
+
+          cur = d.getCurrent();
+          cx = cur.getX();
+          cy = cur.getY();
+
+          gc.fillOval(cur.getX() - (DOT_WIDTH /2), cur.getY() - (DOT_WIDTH /2), DOT_WIDTH, DOT_WIDTH);
+
+          gc.setStroke(Color.DARKRED);
+          for (final Dot e : dots) {
+            final Point2D sub;
+            final double  sx, sy;
+
+            sub = e.getCurrent();
+            sx = sub.getX();
+            sy = sub.getY();
+
+            if (cur.distance(sub) <= MAX_LINE_LENGTH) {
+              gc.strokeLine(cx, cy, sx, sy);
+              connections++;
+              if (connections >=  MAX_CONNECTIONS) break;
+            }
+          }
+        }
+      }
+    };
+
+    timer.start();
+  }
+}
---
+++ src/io/vulpine/dots/Dot.java
@@ -0,0 +1,53 @@
+package io.vulpine.dots;
+
+import javafx.geometry.Point2D;
+
+import java.util.Random;
+
+public class Dot
+{
+  protected final static Random rand = new Random();
+
+  protected Point2D current;
+  protected final Point2D start;
+  protected final double angle;
+  protected final boolean back;
+  protected final double speed;
+  protected final double opacity;
+
+  public Dot()
+  {
+    final Point2D end;
+    final int r = (int) randomDouble(0, 3);
+
+    start = new Point2D(randomDouble(0, Application.MAX_X), randomDouble(0, Application.MAX_Y));
+    speed = randomDouble(0.2, 1.5);
+    opacity = randomDouble(0.2, 1);
+
+    back  = randomDouble(-10, 10) < 0;
+
+    current = new Point2D(start.getX(), start.getY());
+    angle   = randomDouble(0, 3.14);
+  }
+
+  public static double randomDouble(double min, double max) { return min + (max - min) * rand.nextDouble(); }
+
+  public void step()
+  {
+    final double x, y;
+    x = Math.sin(angle) * speed;
+    y = Math.cos(angle) * speed;
+
+    current = back ? current.subtract(x, y) : current.add(x, y);
+  }
+
+  public Point2D getCurrent()
+  {
+    return current;
+  }
+
+  public double getOpacity()
+  {
+    return opacity;
+  }
+}
