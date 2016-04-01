ba5a9afc5eca050d795e450437edc5ebf65af87d
Ellie Harper <elliefops@gmail.com>
2016-03-31 23:22:33 -0400

    Splitting

--- src/io/vulpine/dots/Application.java
+++ src/io/vulpine/dots/Application.java
@@ -6,10 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.BlendMode;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import static java.lang.Math.sqrt;
 
 public class Application extends javafx.application.Application
 {
@@ -31,8 +28,8 @@ public class Application extends javafx.application.Application
     final Canvas          canvas1, canvas2;
     final Star[]          stars;
     final GraphicsContext con1, con2;
-    final Group           g;
-    final Scene           s;
+    final Group           group;
+    final Scene           scene;
     final AnimationTimer timer;
 
     stage.setTitle("Stars");
@@ -45,65 +42,19 @@ public class Application extends javafx.application.Application
 
     con1.setGlobalBlendMode(BlendMode.SRC_OVER);
 
-    g = new Group(canvas2);
-    g.getChildren().add(canvas1);
+    group = new Group(canvas2);
+    scene = new Scene(group);
 
-    s = new Scene(g);
+    group.getChildren().add(canvas1);
 
     stage.setWidth(800);
     stage.setHeight(600);
-    stage.setScene(s);
+    stage.setScene(scene);
     stage.show();
 
     for (int i = 0; i < DOT_COUNT; i++) stars[i] = new Star();
 
-    timer = new AnimationTimer() {
-
-      @Override
-      public void handle( final long now )
-      {
-        con1.clearRect(0, 0, MAX_X, MAX_Y);
-
-        con2.setFill(Color.hsb(Star.getHue()+180, Star.getSaturation(), Star.getBrightness()));
-
-        con2.fillRect(0, 0, MAX_X, MAX_Y);
-
-        // Position
-        for (int i = 0; i < DOT_COUNT; i++) {
-          final double  x, y;
-          stars[i].step();
-
-          x = stars[i].getPosX();
-          y = stars[i].getPosY();
-
-          if (x > MAX_X || x < 0 || y > MAX_Y || y < 0) stars[i] = new Star();
-        }
-
-        // Render
-        for (Star d : stars) {
-          final double cx, cy;
-          int connections = 0;
-
-          con1.setGlobalAlpha(d.getOpacity());
-
-          cx = d.getPosX();
-          cy = d.getPosY();
-
-          for (final Star e : stars) {
-            final double  sx, sy;
-
-            sx = e.getPosX();
-            sy = e.getPosY();
-            if (sqrt((cx-sx)*(cx-sx)+(cy-sy)*(cy-sy)) <= MAX_LINE_LENGTH) {
-              con1.strokeLine(cx, cy, sx, sy);
-              connections++;
-              if (connections >=  MAX_CONNECTIONS) break;
-            }
-          }
-          d.drawStar(con1);
-        }
-      }
-    };
+    timer = new StarTimer(con1, con2, stars);
     timer.start();
   }
 }
---
+++ src/io/vulpine/dots/StarTimer.java
@@ -0,0 +1,65 @@
+package io.vulpine.dots;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+import static io.vulpine.dots.Application.*;
+import static java.lang.Math.sqrt;
+
+public class StarTimer extends AnimationTimer
+{
+  private final GraphicsContext topLayer;
+  private final GraphicsContext bottomLayer;
+  private final Star[]          stars;
+
+  public StarTimer( final GraphicsContext con1, final GraphicsContext con2, final Star[] stars )
+  {
+    this.topLayer = con1;
+    this.bottomLayer = con2;
+    this.stars = stars;
+  }
+
+  @Override
+  public void handle( final long now )
+  {
+    topLayer.clearRect(0, 0, MAX_X, MAX_Y);
+    bottomLayer.setFill(Color.hsb(Star.getHue()+180, Star.getSaturation(), Star.getBrightness()));
+    bottomLayer.fillRect(0, 0, MAX_X, MAX_Y);
+
+    // Position
+    for (int i = 0; i < DOT_COUNT; i++) {
+      final double  x, y;
+      stars[i].step();
+
+      x = stars[i].getPosX();
+      y = stars[i].getPosY();
+
+      if (x > MAX_X || x < 0 || y > MAX_Y || y < 0) stars[i] = new Star();
+    }
+
+    // Render
+    for (Star d : stars) {
+      final double
+        cx = d.getPosX(),
+        cy = d.getPosY();
+      int connections = 0;
+
+      // Set opacity for drawing lines and this star.
+      topLayer.setGlobalAlpha(d.getOpacity());
+
+      for (final Star e : stars) {
+        final double
+          sx = e.getPosX(),
+          sy = e.getPosY();
+
+        if (sqrt((cx-sx)*(cx-sx)+(cy-sy)*(cy-sy)) <= MAX_LINE_LENGTH) {
+          topLayer.strokeLine(cx, cy, sx, sy);
+          connections++;
+          if (connections >=  MAX_CONNECTIONS) break;
+        }
+      }
+      d.drawStar(topLayer);
+    }
+  }
+}
